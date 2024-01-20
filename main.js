e0a10680e197719f51734c7a35732455e353b41e
Vigneshvicky <104623237+vicky3193@users.noreply.github.com>
2024-01-20 13:36:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function menuclick ()  {
+    var menu = document.getElementById ('mobileMenu');
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove ('hidden');
+    } else {
+        menu.classList.add ('hidden');
+    }
+}
